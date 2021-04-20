e5e3982c8e16031039f1d0eda77cb03b1cdd8eb7
noha_m_elmandoh@outlook.com <noha_m_elmandoh@outlook.com>
2021-04-20 05:09:18 +0200

    rev user_api

--- database/migrations/2021_03_11_213939_add_provider_to_users_table.php
+++ database/migrations/2021_03_11_213939_add_provider_to_users_table.php
@@ -14,9 +14,9 @@ class AddProviderToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('provider')->nullable();
-            $table->string('provider_id')->nullable();
-            $table->string('provider_token')->nullable();
+            // $table->string('provider')->nullable();
+            // $table->string('provider_id')->nullable();
+            // $table->string('provider_token')->nullable();
         });
     }
 
@@ -28,9 +28,9 @@ class AddProviderToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('provider');
-            $table->dropColumn('provider_id');
-            $table->dropColumn('provider_token');
+            // $table->dropColumn('provider');
+            // $table->dropColumn('provider_id');
+            // $table->dropColumn('provider_token');
 
 
         });
